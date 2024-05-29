400d27eaa748a09fa81da9ffa76cc3c733c2b8a5
Practisework <Acplwork77@gmail.com>
2024-05-29 16:28:15 +0530

    new file:   components/ContactForm.jsx 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/index.jsx 	renamed:    public/images/WhatsApp_Image_2024-04-19_at_11.51.34_AM-removebg-preview.png -> public/images/Palti .png

---
+++ components/ContactForm.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+
+const Contactform = () => {
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [phone, setPhone] = useState("");
+  let [prod, setProd] = useState("");
+  let [addr, setAddr] = useState("");
+
+  const Submitform = async (event) => {
+    event.preventDefault();
+    fetch(
+      "https://testing-7f0a5-default-rtdb.asia-southeast1.firebasedatabase.app/userData.json",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          Name: name,
+          Email: email,
+          Phone_no: phone,
+          Product: prod,
+          Address: addr,
+        }),
+      }
+    ).then(() => {
+      window.alert("आम्ही तुम्हाला थोड्या वेळा नंतर कॉल करू");
+    });
+  };
+
+  return (
+    <>
+      <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div class="lg:w-1/2 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <iframe
+              width="100%"
+              height="100%"
+              class="absolute inset-0"
+              frameborder="0"
+              title="map"
+              marginheight="0"
+              marginwidth="0"
+              scrolling="no"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d469.02291983830975!2d73.97167372163274!3d19.8744779807339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddbb3ed4ee3d55%3A0xe3d1b5b9fccd4259!2sSHAAN%20AGRO%20(SINNAR)!5e0!3m2!1sen!2sin!4v1716979971284!5m2!1sen!2sin"
+            ></iframe>
+            <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+              <div class="lg:w-1/2 px-6">
+                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  ADDRESS
+                </h2>
+                <p class="mt-1">
+                  Photo booth tattooed prism, portland taiyaki hoodie neutra
+                  typewriter
+                </p>
+              </div>
+              <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <a class="text-indigo-500 leading-relaxed">example@email.com</a>
+                <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
+                <p class="leading-relaxed">123-456-7890</p>
+              </div>
+            </div>
+          </div>
+          <div class="lg:w-1/2 md:w-1/3 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <div class="relative mb-4">
+              <label for="name" class="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div class="relative mb-4">
+              <label for="name" class="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div class="relative mb-4">
+              <label for="email" class="leading-7 text-sm text-gray-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div class="relative mb-4">
+              <label for="name" class="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div class="relative mb-4">
+              <label for="name" class="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button
+              onClick={() => Submitform}
+              class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              Button
+            </button>
+            <p class="text-xs text-gray-500 mt-3">
+              Chicharrones blog helvetica normcore iceland tousled brook viral
+              artisan.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Contactform;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
 import { animate, motion } from "framer-motion";
 import Image from "next/image";
 import GarudPoloRotaryTiller from "../public/images/GarudPoloRotaryTiller.jpeg";
-
+import GarudPlusRotaryTiller from "../public/images/GarudPlusRotaryTiller.jpeg";
+import GarudBalerPolo from "../public/images/GarudBalerPolo.jpeg";
+import Contactform from "@/components/ContactForm";
 const animationVariants = {
   animate: {
     y: [0, 30, 0], // Fade in, fade out, fade in (loop)
@@ -26,7 +28,7 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.7 }}
-            className="bg-main text-center py-3 px-4 text-lg my-10 mx-32 shadow-mainShadow text-white"
+            className="bg-main text-center py-3 px-4 text-lg mt-10 mx-32 shadow-mainShadow text-white"
           >
             अधिक औजारे बघा
           </motion.button>
@@ -49,32 +51,30 @@ export default function Home() {
       </motion.div>
       <section class="text-gray-600 body-font">
         <h1 className="text-3xl text-black text-center">शेतीचे औजारे</h1>
-        <div class="container px-5 py-24 mx-auto">
+        <div class="container px-5 py-8 mx-auto">
           <div class="flex flex-wrap -m-4">
             {/* /////////////////////// da */}
             <div class="p-4 md:w-1/3">
               <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <Image
-                  class="lg:h-48 md:h-36 w-full object-cover object-center"
+                  class="lg:h-72 md:h-36 w-full object-cover object-center"
                   src={GarudPoloRotaryTiller}
                   height={350}
                   width={720}
                   alt="blog"
                 />
                 <div class="p-6">
-                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                    CATEGORY
-                  </h2>
-                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                    Shooting Stars
+                  <h1 class="title-font text-2xl font-medium text-gray-900 mb-3">
+                    गरुड पोलो रोटरी टील्लर
                   </h1>
                   <p class="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
+                    "एक गियर-बॉक्स आहे ज्यामुळे ते संतुलित राहते आणि कंपन होत
+                    नाही. विशेष डिझाइन केलेले ब्लेडचा वापर केल्याने ट्रॅक्टरवरचा
+                    भार कमी होतो, डिझेलची बचत होते आणि टायर घसरत नाहीत."
                   </p>
                   <div class="flex items-center flex-wrap ">
-                    <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                      Learn More
+                    <a class="text-main cursor-pointer text-xl inline-flex items-center md:mb-2 lg:mb-0">
+                      अधिक माहिती बघा
                     </a>
                   </div>
                 </div>
@@ -83,26 +83,24 @@ export default function Home() {
             <div class="p-4 md:w-1/3">
               <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <Image
-                  class="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src={GarudPoloRotaryTiller}
+                  class="lg:h-72 md:h-36 w-full object-cover object-center"
+                  src={GarudPlusRotaryTiller}
                   height={400}
                   width={720}
                   alt="blog"
                 />
                 <div class="p-6">
-                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                    CATEGORY
-                  </h2>
-                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                    Shooting Stars
+                  <h1 class="title-font text-2xl font-medium text-gray-900 mb-3">
+                    गरुड प्लस रोटरी टील्लर
                   </h1>
-                  <p class="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
-                  </p>
+                  "रोटाव्हेटर हे उच्च कार्यक्षमतेवर, डिझाइनवर आणि कार्यक्षमतेवर
+                  भर देणारे यंत्र आहे. जलद आणि स्वस्त मशागतीसाठी हे उत्तम पर्याय
+                  आहे. हेलिकल आकाराच्या फळ्या ट्रॅक्टरवर कमी भार टाकतात,
+                  त्यामुळे ते वापरण्यास अधिक स्वस्त आहे.
+                  <p class="leading-relaxed mb-3"></p>
                   <div class="flex items-center flex-wrap ">
-                    <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                      Learn More
+                    <a class="text-main cursor-pointer text-xl inline-flex items-center md:mb-2 lg:mb-0">
+                      अधिक माहिती बघा
                     </a>
                   </div>
                 </div>
@@ -111,26 +109,26 @@ export default function Home() {
             <div class="p-4 md:w-1/3">
               <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <Image
-                  class="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src={GarudPoloRotaryTiller}
+                  class="lg:h-72 md:h-36 w-full object-cover object-center"
+                  src={GarudBalerPolo}
                   height={400}
                   width={720}
                   alt="blog"
                 />
                 <div class="p-6">
-                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                    CATEGORY
-                  </h2>
-                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                    Shooting Stars
+                  <h1 class="title-font text-2xl font-medium text-gray-900 mb-3">
+                    गरुड बेलर पोलो
                   </h1>
                   <p class="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
+                    "दर हंगामात मोठ्या प्रमाणात सिलेज, हय आणि स्ट्रॉ बेल्स तयार
+                    करण्यासाठी यंत्राच्या तज्ञानाने केलेल्या अभियांत्रिकीची
+                    आवश्यकता असते. पिकांना नुकसान न करता, त्यांचे समान आकार आणि
+                    घनतेमध्ये दाबण्यासाठी बेलिंग प्रक्रिया शक्य तितक्या प्रभावी
+                    असणे आवश्यक आहे.",
                   </p>
                   <div class="flex items-center flex-wrap ">
-                    <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                      Learn More
+                    <a class="text-main text-xl cursor-pointer inline-flex items-center md:mb-2 lg:mb-0">
+                      अधिक माहिती बघा
                     </a>
                   </div>
                 </div>
@@ -139,6 +137,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <Contactform />
     </>
   );
 }
